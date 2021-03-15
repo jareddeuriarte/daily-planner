6935a699c94e65cac88a74e36d5e5e09019edf5b
Jared de Uriarte <jared.deuri@gmail.com>
2021-03-15 18:33:13 -0400

    textarea and save buttons saving to localstorage and displaying in textarea

--- assets/script.js
+++ assets/script.js
@@ -1,25 +1,97 @@
-// var currentDay = document.getElementById("currentDay");
+// WHEN I open the planner
+// THEN the current day is displayed at the top of the calendar ✅
 var today = moment();
-
 $("#currentDay").text(today.format("MMM Do, YYYY"));
 
 
 
-
-// WHEN I open the planner
-// THEN the current day is displayed at the top of the calendar
-
-// WHEN I scroll down
-// THEN I am presented with timeblocks for standard business hours
-
 // WHEN I view the timeblocks for that day
-// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+// THEN 
 
-// WHEN I click into a timeblock
-// THEN I can enter an event
+$()
 
-// WHEN I click the save button for that timeblock
-// THEN the text for that event is saved in local storage
+
+
+
+$(document).ready(function () {
+
+    // listen for save button clicks
+    $('.saveBtn').on('click', function () {
+        // Grabbing the values from text area and hour
+        var comment = $(this).siblings(".description").val()
+        // console.log(comment)
+        var hour = $(this).siblings(".hour").text()
+        // console.log(hour)
+        localStorage.setItem(hour, comment);
+    })
+
+    //Retriving from localstorage and the setting value for 9:00 am in the text area
+    console.log(localStorage.getItem("9:00 AM"));
+    $("#comment9am").val(localStorage.getItem("9:00 AM"));
+    $("#comment10am").val(localStorage.getItem("10:00 AM"));
+
+    // get current number of hours
+    var currentHour = moment().format("h:mm A");
+    console.log(currentHour)
+
+    //Each timeblock is color coded to indicate whether it is in the past, present, or future
+    $(".time-block").each(function(){
+        var hour = $(this).find(".hour").text()
+        console.log(hour)
+        if (hour < currentHour) {
+            $(this).addClass('past');
+          } else if (hour === currentHour) {
+            $(this).removeClass('past');
+            $(this).addClass('present');
+          } else {
+            $(this).removeClass('past');
+            $(this).removeClass('present');
+            $(this).addClass('future');
+          }
+            
+
+    })
+
+    
+});
+
+
+
+
+
+
+
+// // Store
+// // Retrieve
+// document.getElementById("result").innerHTML = localStorage.getItem("lastname");
+
+// // $( "button" ).on( "click", textarea.val(), function( event ) {
+// //     event.preventDefault();
+// //     console.log( $( this ).text("did this work?") );
+// // });
+
+// // $(document).ready(function(){
+// //     $("button").click(function(){
+// //         var comment = $.trim($("#comment9am").val());
+// //         if(comment != ""){
+// //             // Show alert dialog if value is not blank
+// //             alert(comment);
+// //         }
+// //     });
+    
+// // });
+    
+
+
+
+
+
+
+
+
+
+
+
 
 // WHEN I refresh the page
 // THEN the saved events persist
